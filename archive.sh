5bce305ea81546a99c845f3b2e967b2b5f16d4cc
ytq <ytq@hydro-stationcom>
2026-07-12 17:53:05 +0800

    代码评审整改：路径变量统一、完善删除日志、捕获退出信号

--- archive.sh
+++ archive.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#统一定义根目录，消除硬编码问题（评审意见1）
 BASE\_DIR="$HOME/桌面/hydro-data-project"
 DATA\_DIR="${BASE\_DIR}/hydro-data"
 ARCHIVE\_DIR="${BASE\_DIR}/archive"
@@ -20,5 +21,6 @@ else
     write\_log "打包${MONTH}月份数据失败"
 fi
 
+#清理90天过期文件，并且写入日志（评审意见2）
 find ${ARCHIVE\_DIR} -name "data\_\*.tar.gz" -mtime +90 -delete
 write\_log "清理90天之前过期归档文件执行完毕"
